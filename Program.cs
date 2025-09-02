154a0338d33c4667236d3e63e7ff93bee844e031
mhdkadour <mohamad.kadour1234@gmail.com>
2025-09-02 14:58:01 +0200

    uppdatera text

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿namespace ÖvningVecka2
 {
     internal class Program
-    {
+    {//
         static void Main(string[] args)
         {
             Console.WriteLine("Hej, Alla!");
+       
+            Console.WriteLine("Hur är läget!");    
         }
     }
 }
